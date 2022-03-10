e1bda8ef22cf40157a89d601955d2ffbe6f67f65
fbr <florian.brohm@stud.uni-frankfurt.de>
2022-03-10 16:40:17 +0100

    minor: made finding of input child node more flexible

--- static/js/base.js
+++ static/js/base.js
@@ -22,7 +22,7 @@ function get(path, query, timeout = 60000) {
 function loadDir(dir, view) {
     get("https://" + h + "/dirView", { dir }).then(xhr => {
         view.innerHTML = xhr.responseText;
-        view.setAttribute("dir", view.childNodes[1].value);
+        view.setAttribute("dir", view.querySelector("input").value);
     }).catch((e, xhr) => {
         console.log(e);
     });
