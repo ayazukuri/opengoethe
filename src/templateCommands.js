bfd01c67148e30365304653a1691716d66ce3014
fbr <florian.brohm@stud.uni-frankfurt.de>
2022-03-14 00:05:18 +0100

    feat: rtks22 page, google drive view template command

--- src/templateCommands.js
+++ src/templateCommands.js
@@ -35,4 +35,18 @@ function ti(...title) {
     return `<header><h1><a href="/">Informatik Goethe</a>${t}</h1></header>`;
 }
 
-module.exports = { dir_view: dirView, ti };
+/**
+ * Builds a Google driver folder widget.
+ * @param {string} folderId Google drive folder id.
+ * @param {string} display What kind of display to use. Either "grid" or "list".
+ * @return {string} HTML string containing widget.
+ */
+function driveView(folderId, display) {
+    return `<iframe src="https://drive.google.com/embeddedfolderview?id=${folderId}#${display}" style="width:100%; height:350px; border:0;"></iframe>`;
+}
+
+module.exports = {
+    dir_view: dirView,
+    ti,
+    drive_view: driveView
+};
